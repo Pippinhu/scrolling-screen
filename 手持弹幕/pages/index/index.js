30d276c5170b439b1dea41126a11d08d0db07460
Pippin <41884744+Pippinhu@users.noreply.github.com>
2019-01-23 03:46:44 -0600

    Add files via upload

---
+++ 手持弹幕/pages/index/index.js
@@ -0,0 +1,19 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    color:'#242425',
+    text: '我想大声说',
+    items:[
+      {value:"快"},
+      {value:"中"},
+      {value:"慢"},
+    ]
+  },
+  onLoad: function () {
+    
+  },
+
+})
